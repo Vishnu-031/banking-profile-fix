001f5e486c7e5f8aa7ea4e40186e095afd646b8d
vishnu <vishnucms031@gmail.com>
2025-07-29 12:22:08 +0530

    added new text and button view

--- src/welcome.js
+++ src/welcome.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
+const handleClick = () => {
+  alert("View page");
+};
+
 const Welcome = () => {
-
-return(
+  return (
     <div>
-<h1>
-    Welcome Banking Project 
-</h1>
+      <h1>Welcome Banking Project</h1>
+      <p>This is dummy project and it's just a testing project for me</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
-);
-
+  );
 };
 
 export default Welcome;
